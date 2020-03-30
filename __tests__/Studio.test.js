f2143bd0714056281d3f1ad2c8e5d679d449b0c3
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-03-29 21:38:32 -0700

    added a test and data-helper.js

--- __tests__/Studio.test.js
+++ __tests__/Studio.test.js
@@ -1,20 +1,51 @@
-require('dotenv').config();
+const {
+  getStudio,
+  getStudios,
+} = require('../db/data-helpers');
 
-//const request = require('supertest');
-//const app = require('../lib/app');
-const connect = require('../lib/utils/connect');
-const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('app routes', () => {
-  beforeAll(() => {
-    connect();
+describe('studio routes', () => {
+  it('creates a studio', () => {
+    return request(app)
+      .post('/api/v1/studios')
+      .send({
+        name: 'Grey Matter Studios',
+        address: '1234 studio park',
+        city: 'Los Angeles',
+        state: 'CA',
+        zip: 12345,
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Grey Matter Studios',
+          address: '1234 studio park',
+          city: 'Los Angeles',
+          state: 'CA',
+          zip: 12345,
+          __v: 0
+        });
+      });
   });
+});
+it('gets an ingredient by id', async() => {
+  const studio = await getStudio();
 
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
+  return request(app)
+    .get(`/api/v1/studios/${studio._id}`)
+    .then(res => {
+      expect(res.body).toEqual(studio);
+    });
+});
 
-  afterAll(() => {
-    return mongoose.connection.close();
-  });
+it('gets all studios', async() => {
+  const studios = await getStudios();
+
+  return request(app)
+    .get('/api/v1/studios')
+    .then(res => {
+      expect(res.body).toEqual(studios);
+    });
 });
