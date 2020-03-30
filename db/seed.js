4fc502725a43db5d102c287165e9b7d0ad3dfe64
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-03-29 22:45:14 -0700

    updated comments

--- db/seed.js
+++ db/seed.js
@@ -1,15 +1,14 @@
 const Studio = require('../lib/models/Studio');
 const chance = require('chance').Chance();
 
-// specifying the number of blogs to create with our seed function
+// specifying the number of studio to create with our seed function
 module.exports = async({ studiosToCreate = 10 } = {}) => {
-  // creating blogs
-  // creating an array of blogsToCreate length
+  // creating studios
+  // creating an array of studioToCreate length
   // map through the array
-  // -> for each item in the array we create an object with { author, title, content }
-  // for each blog in the mapped array we create a blog in our mongodb
+  // -> for each item in the array we create an object with studio
+  // for each studio in the mapped array we create a studio in our mongodb
   const studios = await Studio.create([...Array(studiosToCreate)].map(() => ({
-    name: chance.unique(chance.animal),
-    content: chance.paragraph({ words: 3 })
+    name: chance.unique(chance.animal, { words: 3 }),
   })));
 };
